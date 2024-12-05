af22589a08825a7777ef06e18f755e9cec82b233
Akiwork1252 <aki1252work@gmail.com>
2024-12-05 21:12:31 +0900

    update store

--- class/my-lesson/pachinko/eva.py
+++ class/my-lesson/pachinko/eva.py
@@ -1,2 +1,61 @@
-def eva():
-    print('CRエヴァンゲリオンで遊びます。')
+import random
+
+# CRエヴァンゲリオン:
+# (通常時の大当り確率): 1/319, (初当たり出玉): +450(75%) or +1500(25%),
+# (初当たり振り分け): 確率変動:70%, チャンスタイム(100回転:大当り確率1/170):30%, (確率変動時の大当り確率): 当選:1/90 回転数:170回転,
+# (確率変動時の出玉振り分け): ALL+1500,
+
+# 抽選（通常時）
+def lottery():
+    probability_of_winning = 319  # 大当り確率
+    n = random.randint(1, probability_of_winning)  # パチンコ台の乱数
+    user = random.randint(1, probability_of_winning)  # ユーザーの乱数
+    # ユーザー乱数＝パチンコ台乱数 >> 大当り
+    if user == n:
+        # 70%で確率変動突入
+        if user <= (probability_of_winning * 0.7):
+            # 大当りのうちの25%がビッグボーナス (+1500玉)
+            if user <= ((probability_of_winning * 0.7) * 0.25):
+                big_bonus = 1500
+                print('***BIG BONUS(+1500玉)ゲット*** >>> 確率変動突入!(170回転)')
+                return big_bonus
+            else:
+                bonus = 450
+                print('***BONUS(+450玉)ゲット*** >>> 確率変動突入!(170回転)')
+                return bonus
+        else:
+            bonus = 450
+            print('***BONUS(+450玉)ゲット*** >>> チャンスタイム突入!(100回転)')
+            return float(bonus)  # チャンスタイムであればfloat型で返す
+    else:
+        print('-', end='')
+        return None
+
+# 抽選（チャンスタイム）
+def lottery_chance():
+    probability_of_winning = 170
+    n = random.randint(1, probability_of_winning)  # パチンコ台の乱数
+    user = random.randint(1, probability_of_winning)  # ユーザーの乱数
+    # ユーザー乱数＝パチンコ台乱数 >> 大当り+確率変動突入
+    if user == n:
+        big_bonus = 1500
+        print('***BIG BONUS(+1500玉)ゲット*** >>> 確率変動突入!(170回転)')
+        return big_bonus
+    else:
+        print('-', end='')
+        return None
+
+# 抽選（確率変動時） 大当り(1/90)
+def lottery_bonus():
+    winning = 90  # 大当り確率
+    n_winning = random.randint(1, winning)  # パチンコ台の大当り乱数
+    user = random.randint(1, winning)  # ユーザーの乱数
+    # ユーザー乱数＝パチンコ台当たり乱数 >> 大当り(+1500玉)
+    if user == n_winning:
+        big_bonus = 1500
+        input('*BIG BONUS(+1500玉)* を引きました。>>> 確率変動継続!(170回転) Enterキーを押してください。')
+        return big_bonus
+    else:
+        print('-', end='')
+        return None
+
--- class/my-lesson/pachinko/hokuto.py
+++ class/my-lesson/pachinko/hokuto.py
@@ -1,25 +1,25 @@
 import random
 
-# スペック:CR北斗の拳:
-# (通常時の大当り確率): 1/349, (初当たり出玉): +300(4/5) or +1500(1/5),
+# CR北斗の拳:
+# (通常時の大当り確率): 1/349, (初当たり出玉): +300(80%) or +1500(20%),
 # (確率変動突入率): 100%, (確率変動時の大当り確率): 当たり:1/40, 転落:1/150,
-# (確率変動時の出玉振り分け): +300(1/4) or +1500(3/4),
+# (確率変動時の出玉振り分け): +300(25%) or +1500(75%),
 
 # 抽選（通常時）
-def lottery(): # count:回転数
+def lottery():
     probability_of_winning = 349  # 大当り確率
     n = random.randint(1, probability_of_winning)  # パチンコ台の乱数
     user = random.randint(1, probability_of_winning)  # ユーザーの乱数
     # ユーザー乱数＝パチンコ台乱数 >> 大当り
     if user == n:
-        # ５分の１でビッグボーナス (+1500玉）
-        if user <= (probability_of_winning / 5):
+        # 20%でビッグボーナス (+1500玉）
+        if user <= (probability_of_winning * 0.2):
             big_bonus = 1500
-            print('***BIG BONUS(+1500玉)ゲット*** <おめでとうございます>')
+            print('***BIG BONUS(+1500玉)ゲット*** >>> 確率変動突入!')
             return big_bonus
         else:
             bonus = 300
-            print('***BONUS(+300玉)ゲット*** <おめでとうございます>')
+            print('***BONUS(+300玉)ゲット*** >>> 確率変動突入!')
             return bonus
     else:
         print('-', end='')
@@ -34,14 +34,14 @@ def lottery_bonus():
     user = random.randint(1, winning)  # ユーザーの乱数
     # ユーザー乱数＝パチンコ台当たり乱数 >> 大当り
     if user == n_winning:
-        # ４分の３でビッグボーナス
-        if user >= (winning / 4):
+        # 75%がビッグボーナス
+        if user <= (winning * 0.75):
             big_bonus = 1500
-            print('*BIG BONUS(+1500玉)* を引きました。 >>> 確率変動継続', end=' ')
+            input('*BIG BONUS(+1500玉)* を引きました。>>> 確率変動継続! Enterキーを押してください。')
             return big_bonus
         else:
             bonus = 300
-            print('*BONUS(+300玉)* を引きました。>>> 確率変動継続', end=' ')
+            input('*BONUS(+300玉)* を引きました。>>> 確率変動継続! Enterキーを押してください。')
             return bonus
     # ユーザー乱数＝パチンコ台転落乱数 >> 通常に転落
     elif user == n_losing:
