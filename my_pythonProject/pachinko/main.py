a16eee30956f89a31bf335843f62061d7e1ede69
Akiwork1252 <aki1252work@gmail.com>
2024-12-11 07:50:22 +0900

    update gitignore and made operate-db files

--- my_pythonProject/pachinko/main.py
+++ my_pythonProject/pachinko/main.py
@@ -1,40 +1,55 @@
-from store import Store
-from store import Pachinko
+import sqlite3
+from operate_db import DB
+from store import Store, Pachinko
 
 
+# メインクラス
+# 入店（アカウント確認・作成）、遊技関数の呼び出し、退店（遊技記録をDBに保存）
 class Main:
-    # 各機種の関数ディクショナリー
+    # 遊技台の関数ディクショナリー
     func_dict = {
         'H': Pachinko.hokuto,
         'E': Pachinko.eva,
         'M': Pachinko.madomagi,
     }
     play_dict = {}
-    user_info_list = []
 
     @staticmethod
-    def account_create():
-        print('名前,年齢,所持金を入力してください。')
+    def account_check():
+        print('名前,年齢,所持金を半角英数字で入力してください。')
         name = input('名前: ')
-        age = int(input('年齢： '))
-        money = int(input('所持金: '))
-        Main.user_info_list.append(age)
-        Main.user_info_list.append(money)
-        Main.play_dict[name] = Main.user_info_list
+        while True:
+            try:
+                age = int(input('年齢： '))
+            except ValueError:
+                print('半角英数字で入力してください。(例:32)')
+            else:
+                break
+        # 名前と年齢でデータベースを検索して、データがあれば、前回の所持金を返す。データが無ければ作成する。
+        user = DB.user_search_and_add(name, age)  # 戻り値 >>> データあり:int(money)、データなし:tuple(name, age, money)
+        if type(user) is tuple:
+            # char:インスタンス作成
+            char = Store(user[0], user[1], user[2])
+        else:
+            # char:インスタンス作成(前回の所持金を利用)
+            char = Store(name, age, user)
         # char:インスタンス作成
-        char = Store(name, age, money)
+        print('下記のアカウントで入店します。')
         print('-'*20)
-        print('アカウントを作成しました。')
-        print(f'名前： {char.name}')
-        print(f'年齢： {char.age}')
-        print(f'所持金： {char.money}')
+        print(f'名前: {char.name}')
+        print(f'年齢: {char.age}歳')
+        print(f'所持金: {char.money}円')
         print('-'*20)
-
+        while True:
+            action = input('確認したらEnterキーを押してください。')
+            if type(action) is str:
+                break
         return char
 
     @staticmethod
     def main():
-        char = Main.account_create()
+        DB.create_table()  # テーブルの作成（初回のみ）
+        char = Main.account_check()
         judge = char.entrance()
         if judge is None:
             print('またのご来店をお待ちしております。')
@@ -51,4 +66,4 @@ class Main:
 
 
 Main.main()
-# Main.account_create()
+
---
+++ my_pythonProject/pachinko/operate_db.py
@@ -0,0 +1,76 @@
+import sqlite3
+import datetime
+
+
+class DB:
+    # テーブル作成(初回のみ）
+    @staticmethod
+    def create_table():
+        # <pachinko.db>
+        # userテーブル(名前、年齢、所持金)
+        # historyテーブル(日付、名前、年齢、遊技台、収支)
+        con = sqlite3.connect('pachinko.db')
+        cursor = con.cursor()
+        cursor.execute('CREATE TABLE IF NOT EXISTS user(name CHAR(20), age INT, money INT)')
+        cursor.execute('CREATE TABLE IF NOT EXISTS history(date CHAR(20), name CHAR(20), '
+                       'age INT, machine CHAR(20), income INT)')
+        con.commit()
+
+    # def del_table():
+    #     con = sqlite3.connect('pachinko.db')
+    #     cursor = con.cursor()
+    #     cursor.execute('DROP TABLE user')
+    #     cursor.execute('DROP TABLE history')
+    #     con.commit()
+
+    # 来店時　ユーザー情報を確認、履歴が無ければデータベースに作成
+    @staticmethod
+    def user_search_and_add(name, age):
+        count = 0
+        con = sqlite3.connect('pachinko.db')  # userTABLE(name, age, money)
+        cursor = con.cursor()
+        itr = cursor.execute('SELECT * FROM user WHERE (name=(?)) and (age=(?))', [name, age])
+        for i in itr:
+            count += 1
+        # WHEREでデータがヒットしない
+        if count == 0:
+            print('初めてのご来店ですね。所持金を入力してください。')
+            while True:
+                try:
+                    money = int(input('所持金: '))
+                    cursor.execute('INSERT INTO user VALUES(?, ?, ?)', [name, age, money])
+                    con.commit()
+                except ValueError:
+                    print('半角英数字で入力してください。(例:5000)')
+                else:
+                    break
+            return name, age, money
+        else:
+            while True:
+                user = input(f'来店履歴が確認できました。前回の所持金{i[2]}円を使用して遊技ができます。Enterキーを押してください。')
+                if type(user) is str:
+                    break
+            return i[2]
+
+    # ユーザーの遊技履歴を表示する
+    @staticmethod
+    def check_history(name, age):
+        count = 0
+        con = sqlite3.connect('pachinko.db')
+        cursor = con.cursor()
+        itr = cursor.execute('SELECT * FROM history WHERE (name == (?)) and (age == (?))', [name, age])
+        for user in itr:
+            count += 1
+            print(user)
+        if count == 0:
+            print('遊技履歴が存在しませんでした。')
+        con.commit()
+
+    # 遊技終了後のデータをテーブル(history)に追加する　{日付、名前、年齢、遊技台、収支}
+    @staticmethod
+    def save_when_exit(date, name, age, machine, income):
+        con = sqlite3.connect('pachinko.db')
+        cursor = con.cursor()
+        cursor.execute('INSERT INTO history VALUES(?, ?, ?, ?, ?)', [date, name, age, machine, income])
+        con.commit()
+
